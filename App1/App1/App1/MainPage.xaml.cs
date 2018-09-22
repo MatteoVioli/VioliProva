05192d29078f1899065be0ac9154adf3f6e17e87
matteovioli00 <matteovioli00@gmail.com>
2018-09-22 09:41:13 +0100

    Implementato codice svolto la lezione precedente ( 17/09/2018 )

--- App1/App1/App1/MainPage.xaml.cs
+++ App1/App1/App1/MainPage.xaml.cs
@@ -5,13 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App1
+namespace TestAndroidApp
 {
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+		void OnButtonClicked(object sender, EventArgs args)
+		{
+			lblSaluto.Text = "Ciao a tutti";
+			int a = Convert.ToInt32(txtNum1.Text);
+			int b = Convert.ToInt32(txtNum2.Text);
+			int somma = a + b;
+			lblRisultato.Text = somma.ToString();
 		}
 	}
 }
